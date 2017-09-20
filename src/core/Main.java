6b4ad079f5ef931da383a23b67285f6ebca17973
Munkhbold.Bold <munkhbold.bold@gabo.de>
2017-09-20 17:21:10 +0200

    prepared dynamic controllers Menu Toolbox Anchored Pane 3

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,6 +1,8 @@
 package core;
 
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,19 +10,31 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     Stage window;
-    Scene sceneLogin, sceneProject, sceneTimesheet;
+    // Scene sceneLogin, sceneProject, sceneTimesheet;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/createTS.fxml"));
         primaryStage.setTitle("Zeitbuchung");
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.sizeToScene();
+        // primaryStage.setResizable(false);
+        primaryStage.setMinHeight(600);
+        primaryStage.setMinWidth(900);
         primaryStage.show();
 
+        // Window width changed
+        primaryStage.widthProperty().addListener((ObservableValue<? extends Number> obs, Number oldVal, Number newVal) -> {
+            System.out.println("Breite: " + oldVal + " " + newVal);
+
+        });
+
+        // Window height changed
+        primaryStage.heightProperty().addListener((ObservableValue<? extends Number> obs, Number oldVal, Number newVal) -> {
+            System.out.println("Höhe: " + oldVal + " " + newVal);
+        });
 
     }
     public static void main(String[] args) {
